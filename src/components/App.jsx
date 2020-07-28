aced861020299b06374b26752094bba4c79b263d
Harold F <harold.f415@gmail.com>
2020-07-27 22:55:18 -0400

    react-hooks-useState completed a lesson on React Hooks and using the useState hook. This allowed me to change the state of a variable that was already rendered on the main webpage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const [count, setCount] = useState(0); // useState hook
+
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
+  }
+  function multiply2() {
+    setCount(count * 2);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+      <button onClick={multiply2}>x</button>
+    </div>
+  );
 }
 
 export default App;
